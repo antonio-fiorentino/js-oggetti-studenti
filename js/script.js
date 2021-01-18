f0fc79003e1a8eb3ef4edec3a43673544b59ecbd
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-01-18 17:09:22 +0100

    Esercizio

--- js/script.js
+++ js/script.js
@@ -10,23 +10,24 @@
 // }
 
 
-//Punto 2
-var studenti = [
-  {'nome': 'Giuliano Sarri'},
-  {'nome': 'Giuliano Sarri'},
-]
-for (var i=0; i<studenti.length; i++){
-  var studente = studenti[i];
-  alert(studente.nome);
-}
+// //Punto 2
+// var studenti = [
+//   {'nome': 'Giuliano', 'cognome':'Sarri'},
+//   {'nome': 'Giuliano Sarri'},
+// ]
+// for (var i=0; i<studenti.length; i++){
+//   var studente = studenti[i];
+//   alert(studente.nome + studente.cognome);
+//
+// }
 
 
-// //Punto 3
-// var studenti = {
-//   'studente': [],
-// };
-// var ragazzo = {}
-// ragazzo.nome = prompt("Qual'è il tuo nome?");
-// ragazzo.cognome = prompt("Qual'è il tuo cognome?");
-// ragazzo.età = prompt('Quanti anni hai?');
-// studenti.studente.push(ragazzo);
+//Punto 3
+var studenti = {
+  'studente': [],
+};
+var ragazzo = {}
+ragazzo.nome = prompt("Qual'è il tuo nome?");
+ragazzo.cognome = prompt("Qual'è il tuo cognome?");
+ragazzo.età = prompt('Quanti anni hai?');
+studenti.studente.push(ragazzo);
