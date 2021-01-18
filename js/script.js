3043408172462f22be191022145a737542cd1c99
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-01-18 15:46:19 +0100

    Esercizio

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// //Punto 1
+// var studente = {
+//   'nome' : 'Giovanni',
+//   'cognome' : 'Muciaccia',
+//   'età' : 51,
+// }
+//
+// for (var key in studente){
+//   alert(studente[key]);
+// }
+
+
+//Punto 2
+var studenti = [{
+  'nome1': 'Giuliano Sarri',
+  'nome2': 'Emilio Fede',
+}]
+for (var key in studenti){
+  alert(studenti[key])
+  console.log(studenti[key])
+}
+
+
+// //Punto 3
+// var studenti = {
+//   'studente': [],
+// };
+// var ragazzo = {}
+// ragazzo.nome = prompt("Qual'è il tuo nome?");
+// ragazzo.cognome = prompt("Qual'è il tuo cognome?");
+// ragazzo.età = prompt('Quanti anni hai?');
+// studenti.studente.push(ragazzo);
